4fd739b01e746834117c4fb61c3c9fbff15bed58
zhili yu <yuzl@broada.com>
2019-04-11 11:02:55 +0800

    init admin server module

---
+++ platform/admin-server/src/main/java/com/timemac/amall/platform/admin/server/AdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.timemac.amall.platform.admin.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+
+}
