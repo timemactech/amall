93544742058df9a61a8a692c2e9c363016934bd9
zhili yu <yuzl@broada.com>
2019-04-11 16:39:45 +0800

    add apollo client and AppConfig

---
+++ micro-service/order/src/main/java/com/timemac/amall/order/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.timemac.amall.order.config;
+
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+
+/**
+ * @author ivanyu
+ */
+@Configuration
+@EnableApolloConfig(value = {"application", "amall.common"}, order = Ordered.HIGHEST_PRECEDENCE)
+public class AppConfig {
+}
