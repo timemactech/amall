ee6f2d3df8ffcb13af6b25a0cee33f74ebb4321e
zhili yu <yuzl@broada.com>
2019-04-12 21:31:03 +0800

    enable admin server

--- platform/admin-server/src/main/java/com/timemac/amall/platform/admin/server/AdminServerApplication.java
+++ platform/admin-server/src/main/java/com/timemac/amall/platform/admin/server/AdminServerApplication.java
@@ -1,11 +1,13 @@
 package com.timemac.amall.platform.admin.server;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableAdminServer
 public class AdminServerApplication {
 
     public static void main(String[] args) {
